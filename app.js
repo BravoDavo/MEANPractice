4b164276251b3d1914fa441efcccff9751854742
BravoDavo <david.jaesch@gmx.de>
2017-02-02 14:31:34 +0100

    node with express setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hallo Node.js!')
+});
+
+const server = http.createServer(app);
+
+    server.listen(3000, () => {
+        console.log('Server listening on port 3000.');
+    });
