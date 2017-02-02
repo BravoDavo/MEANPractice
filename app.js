1e66989a9584f932e4ae30c89310643be8026f81
BravoDavo <david.jaesch@gmx.de>
2017-02-02 16:17:52 +0100

    node with express setup and mongoose examples

--- app.js
+++ app.js
@@ -13,3 +13,105 @@ const server = http.createServer(app);
     server.listen(3000, () => {
         console.log('Server listening on port 3000.');
     });
+
+// Load mongoose package
+var mongoose = require('mongoose');
+// Connect to MongoDB and create/use database called todoAppTest
+mongoose.connect('mongodb://localhost/todoAppTest');
+// Create a schema
+var TodoSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  note: String,
+  updated_at: { type: Date, default: Date.now },
+});
+
+// Mongoose uses JSON for the Data Logic
+// Mongoose Create
+// Create a model based on the schema
+var Todo = mongoose.model('Todo', TodoSchema);
+
+// Create a todo in memory
+var todo = new Todo({name: 'Master NodeJS', completed: false, note: 'Getting there...'});
+// Save it to database
+todo.save(function(err){
+  if(err)
+    console.log(err);
+  else
+    console.log(todo);
+});
+
+// Mongoose Read and Query
+// Find all data in the Todo collection
+Todo.find(function (err, todos) {
+  if (err) return console.error(err);
+  console.log(todos)
+});
+
+// Find with queries
+// callback function to avoid duplicating it all over
+var callback = function (err, data) {
+  if (err) { return console.error(err); }
+  else { console.log(data); }
+}
+// Get ONLY completed tasks
+Todo.find({completed: true }, callback);
+// Get all tasks ending with `JS`
+Todo.find({name: /JS$/ }, callback);
+
+// Chaining queries
+var oneYearAgo = new Date();
+oneYearAgo.setYear(oneYearAgo.getFullYear() - 1);
+// Get all tasks staring with `Master`, completed
+Todo.find({name: /^Master/, completed: true }, callback);
+// Get all tasks staring with `Master`, not completed and created from year ago to now...
+Todo.find({name: /^Master/, completed: false }).where('updated_at').gt(oneYearAgo).exec(callback);
+
+// Another example
+// find each person with a last name matching 'Ghost'
+var query = Person.findOne({ 'name.last': 'Ghost' });
+
+// selecting the `name` and `occupation` fields
+query.select('name occupation');
+
+// execute the query at a later time
+query.exec(function (err, person) {
+  if (err) return handleError(err);
+  console.log('%s %s is a %s.', person.name.first, person.name.last, person.occupation) // Space Ghost is a talk show host.
+})
+
+// With a JSON doc
+Person.
+  find({
+    occupation: /host/,
+    'name.last': 'Ghost',
+    age: { $gt: 17, $lt: 66 },
+    likes: { $in: ['vaporizing', 'talking'] }
+  }).
+  limit(10).
+  sort({ occupation: -1 }).
+  select({ name: 1, occupation: 1 }).
+  exec(callback);
+  
+// Using query builder
+Person.
+  find({ occupation: /host/ }).
+  where('name.last').equals('Ghost').
+  where('age').gt(17).lt(66).
+  where('likes').in(['vaporizing', 'talking']).
+  limit(10).
+  sort('-occupation').
+  select('name occupation').
+  exec(callback);
+
+// Mongoose Update
+// Todo.update and Todo.findOneAndUpdate
+// Model.update(conditions, update, [options], [callback])
+// update `multi`ple tasks from complete false to true
+Todo.update({ name: /master/i }, { completed: true }, { multi: true }, callback);
+// Model.findOneAndUpdate([conditions], [update], [options], [callback])
+Todo.findOneAndUpdate({name: /JS$/ }, {completed: false}, callback);
+
+// Mongoose Delete
+// use remove
+// update and remove are identical
